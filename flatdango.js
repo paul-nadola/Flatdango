05fcbf4467be1319e2098fd2caa28ab05726f84d
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-04-03 12:45:57 +0300

    Completed Assignment

--- flatdango.js
+++ flatdango.js
@@ -1,130 +1,85 @@
 
-function displayDefault(){//A function that displays the first movie in the data base
-        
-    fetch(`http://localhost:3000/films`)//fetching for data
-    .then(res => res.json())//converting the data
-    .then(films => {
-        const currentFilm = document.getElementById('current')//defining the place the default movie will be displayed
-        const defaultFilm = films[0]//Grabbing the first movie
-//Creating the HTML card that will display the details
-        currentFilm.innerHTML = `
-            <div id='defMovie'>
-                <h2>${defaultFilm.title}</h2>
-                <p>Duration: ${defaultFilm.runtime} minutes</p>
-                <p>Maximum viewers: ${defaultFilm.capacity} people</p>
-                <p>Airing at: ${defaultFilm.showtime}</p>
-                <p>Tickets Sold: ${defaultFilm.tickets_sold} tickets</p>
-                <p>Description: ${defaultFilm.description}</p>
-                <img id="image" src="${defaultFilm.poster}">
+
+
+fetch(`http://localhost:3000/films`)
+  .then(res => res.json())
+  .then(films => displayMovies(films))
+  .catch(error => console.error(error));//Fetching data ,passing it in our function
+
+function displayMovies(films) {//declaring our function
+  const currentFilm = document.getElementById('current');
+  const defaultFilm = films[0];
+
+  currentFilm.innerHTML = `
+    <div id='defMovie'>
+      <h2>${defaultFilm.title}</h2>
+      <p>Duration: ${defaultFilm.runtime} minutes</p>
+      <p>Maximum viewers: ${defaultFilm.capacity} people</p>
+      <p>Airing at: ${defaultFilm.showtime}</p>
+      <p>Tickets Sold: ${defaultFilm.tickets_sold} tickets</p>
+      <p>Description: ${defaultFilm.description}</p>
+      <button type="submit" id='buy'>BUY TICKET</button>
+      <img id="image" src="${defaultFilm.poster}">
+    </div>
+  `;
+
+  const cardList = document.getElementById('movieList');//Grabbing elements and declaring variables
+  films.forEach(film => {//iterating through the elements to display titles on the left side
+    const card = document.createElement('li');
+    card.innerHTML = `<a href="#" id="movie-${film.id}">${film.title}</a><p></p>`;
+    cardList.appendChild(card);//Creating a card that displays movie titles
+
+    card.addEventListener('click', (event) => {
+      event.preventDefault();//adding a click event listener that will display movie details when clicked
+      const selectedFilmId = event.target.id.split('-')[1];
+      fetch(`http://localhost:3000/films/${selectedFilmId}`)//fetching data to display movie details
+        .then(res => res.json())
+        .then(selectedFilm => {
+          const currentFilm = document.getElementById('current');
+          //creating a card that will display movie details
+          currentFilm.innerHTML = `
+            <div id='selMovie'>
+              <h2>${selectedFilm.title}</h2>
+              <p>Duration: ${selectedFilm.runtime} minutes</p>
+              <p>Maximum viewers: ${selectedFilm.capacity} people</p>
+              <p>Airing at: ${selectedFilm.showtime}</p>
+              <p>Tickets Sold: ${selectedFilm.tickets_sold} tickets</p>
+              <p>Description: ${selectedFilm.description}</p>
+              <button type="submit" id='buy'>BUY TICKET</button>
+              <img id="image" src="${selectedFilm.poster}">
             </div>
-        `
-    })
-    .catch(error => console.error(error));
-   //a function that will throw any errors encounteres
-}
-
-function displayMovies(){//a function that will display the details of the films when clicked in in the menu section
-
-    fetch(`http://localhost:3000/films`)//fetching data
-    .then(res => res.json())//converting the data
-    .then(films => {
-        const cardList = document.getElementById('movieList');//declaring the parent node
-        const master = document.getElementById('display');
-        
-        films.forEach(film => {//creating an element that will link the menu to the display container
-            const card = document.createElement('li');
-            card.innerHTML = `<a href="#" id="movie-${film.id}">${film.title}</a>`;
-            cardList.appendChild(card);//appending the child node
-
-            // Add event listener to each film title link
-            card.addEventListener('click', (event) => {
-                event.preventDefault()//preventing default reload
-                const selectedFilmId = event.target.id.split('-')[1]//
-                fetch(`http://localhost:3000/films/${selectedFilmId}`)
-                .then(res => res.json())//fetching the data and converting
-                .then(selectedFilm => {
-                    const currentFilm = document.getElementById('current')
-                    //creating the card that will display the details
-                    currentFilm.innerHTML = `
-                    <div id='selMovie'>
-                        <h2>${selectedFilm.title}</h2>
-                        <p>Duration: ${selectedFilm.runtime} minutes</p>
-                        <p>Maximum viewers: ${selectedFilm.capacity} people</p>
-                        <p>Airing at: ${selectedFilm.showtime}</p>
-                        <p>Tickets Sold: ${selectedFilm.tickets_sold} tickets</p>
-                        <p>Description: ${selectedFilm.description}</p>
-                        <img id="image" src="${selectedFilm.poster}">
-                    </div>
-                    `
-                })
-
-                  });
-                  
-                  
-
-      })
-
-    })
-}
-
-displayDefault();//calling the functions to manipulate the DOM and fetch from server
-displayMovies();
-
-
-function buyTickets() {
-  const buyTick = document.getElementById('buy');
-  const dis = document.getElementById('current')
-  
-  buyTick.addEventListener('click', (event) => {
-    event.preventDefault();
-    let tickets_sold = 0; // initialize tickets_sold
-    const selectedFilmId = event.target.id
-    let newSales = tickets_sold + 1;
-    
-    fetch(`http://localhost:3000/films/${selectedFilmId}]`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        tickets_sold: newSales
-      })
-    })
-    .then(res => res.json())
-    .then(data => {
-      // loop over films array and update tickets_sold variable
-      data.forEach(film => {
-        tickets_sold = film.tickets_sold;
-      });
-      return `Tickets sold: ${tickets_sold}`
-    })
-    dis.appendChild(buyTick)
-    .catch(error => console.log(error)); 
+          `;
+          const buyTick = currentFilm.querySelector('#buy');
+          buyTick.addEventListener('click', (event) => {
+            event.preventDefault();//event listener that will handle buy tickets
+            selectedFilm.tickets_sold += 1;
+            fetch(`http://localhost:3000/films/${selectedFilm.id}`, {//Making a patch request to enable the buttons functionality
+              method: 'PATCH',
+              headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+              },
+              body: JSON.stringify({
+                tickets_sold: selectedFilm.tickets_sold
+              })
+            })
+              .then(res => res.json())
+              .then(updatedFilm => {//If statement that tells the user if the purchase is successful or the tickets are sold out
+                if (updatedFilm.tickets_sold < updatedFilm.capacity) {
+                  alert(`You have purchased a ticket for ${updatedFilm.title}! Enjoy the movie for the next ${updatedFilm.runtime} minutes.`);
+                } else {
+                  alert(`Sorry, the movie ${updatedFilm.title} is sold out! Please check out our other movies.`);
+                  const direct = currentFilm.querySelector('p');
+                  direct.textContent = 'SOLD OUT!!!';
+                  // card.appendChild(direct);
+                }
+              })
+              .catch(error => console.log(error));
+          });
+        })
+        .catch(error => console.log(error));
+    });
   });
 }
-
-buyTickets()
-
-        // const noOfTickets = parseInt(document.getElementById('numberOfTickets').value); // convert value to integer - user input on number of tickets
-        // const newTicketsSold = film.tickets_sold + noOfTickets;
-        // const remainingTickets = film.capacity - film.tickets_sold;
-
-        // if (noOfTickets > remainingTickets) {
-        //   alert('Sorry, we are sold out. Kindly try other titles');
-        // } else {
-        //   return newTicketsSold = film.tickets_sold + noOfTickets
-           
-        // }
-
-        // fetch(`http://localhost:3000/films/${currentMovie.value}`, { // use film.id instead of movie.id
-        //   method: 'PATCH',
-        //   headers: {
-        //     'Content-Type': 'application/json'
-        //   },
-        //   body: JSON.stringify({
-        //     tickets_sold: newTicketsSold
-        //   })
-        // })
 
 
